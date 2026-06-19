4920d0870e3e9b1f679b3e224ee6afba2a3e8c6e
reviupikiran-debug <reviupikiran@gmail.com>
2026-06-19 23:09:34 +0700

    Create src/data/links.js
    add internal linking data engine

---
+++ src/data/links.js
@@ -0,0 +1,13 @@
+export const internalLinks = {
+  blog: [
+    { title: "Harga Undangan Digital", url: "/blog/harga-undangan-digital" },
+    { title: "Cara Membuat Undangan Digital", url: "/blog/cara-membuat-undangan-digital" },
+    { title: "Undangan Digital vs Cetak", url: "/blog/undangan-digital-vs-cetak" }
+  ],
+
+  cities: [
+    { title: "Jakarta", url: "/undangan-digital/jakarta" },
+    { title: "Bandung", url: "/undangan-digital/bandung" },
+    { title: "Surabaya", url: "/undangan-digital/surabaya" }
+  ]
+};
